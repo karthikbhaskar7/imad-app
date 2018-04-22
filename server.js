a1e1e4b97502cfa9e88eabab7ef6d4a78d4c15a6
karthikbhaskar7 <33963807+karthikbhaskar7@users.noreply.github.com>
2018-04-22 12:23:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,11 +64,11 @@ app.get('/:articleName', function(req, res){
     res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
