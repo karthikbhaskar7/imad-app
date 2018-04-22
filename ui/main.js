6d87a179830b458fb8d63527bd7bd12fb8b573a6
karthikbhaskar7 <33963807+karthikbhaskar7@users.noreply.github.com>
2018-04-22 11:55:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 // Change body of home page
 var text = document.getElementById('body');
-text.innerHTMl = 'Hi new Value';
+text.innerHTML = 'Hi new Value';
 
 // Move the image
 var img = document.getElementById('img');
