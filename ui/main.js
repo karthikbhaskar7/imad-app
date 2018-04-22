0c0f0b02d8ecdf930ebba9329cab8e7711a8c56b
karthikbhaskar7 <33963807+karthikbhaskar7@users.noreply.github.com>
2018-04-22 12:18:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,2 @@
-console.log('Loaded!');
-
-// Change body of home page
-var text = document.getElementById('body');
-text.innerHTML = 'Hi new Value';
-
-// Move the image
-var img = document.getElementById('img');
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-    
-}
-
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft +'px';
-}
+// Counter funtion
+var counter = 
